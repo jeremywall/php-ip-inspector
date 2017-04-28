31ebbe35d997886ec75b9e9c5ecbdbade0a7dd16
Jeremy Wall <jeremywall@users.noreply.github.com>
2017-04-28 14:51:14 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+echo "<pre>";
+echo "We think the real ip is: " . getRealIP() . "\n";
+echo "\n";
+echo "SERVER\n";
+print_r($_SERVER);
+echo "\n";
+echo "ENV\n";
+print_r($_ENV);
+echo "</pre>";
+
+function getRealIP() {
+	if ($_SERVER['HTTP_X_FORWARDED_FOR'] != '') {
+		if (!empty($_SERVER['REMOTE_ADDR'])) {
+			$client_ip = $_SERVER['REMOTE_ADDR'];
+		} else if (!empty($_ENV['REMOTE_ADDR'])) {
+			$client_ip = $_ENV['REMOTE_ADDR'];
+		} else {
+			$client_ip = 'unknown';
+		}
+		$entries = split('[, ]', $_SERVER['HTTP_X_FORWARDED_FOR']);
+		reset($entries);
+		while (list(, $entry) = each($entries)){
+			$entry = trim($entry);
+			if (preg_match("/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", $entry, $ip_list)){
+				$private_ip = array(
+					'/^0\./',
+					'/^127\.0\.0\.1/',
+					'/^192\.168\..*/',
+					'/^172\.((1[6-9])|(2[0-9])|(3[0-1]))\..*/',
+					'/^10\..*/'
+				);
+				$found_ip = preg_replace($private_ip, $client_ip, $ip_list[1]);
+				if ($client_ip != $found_ip){
+					$client_ip = $found_ip;
+					break;
+				}
+			}
+		}
+	} else {
+		if (!empty($_SERVER['REMOTE_ADDR'])) {
+			$client_ip = $_SERVER['REMOTE_ADDR'];
+		} else if (!empty($_ENV['REMOTE_ADDR'])) {
+			$client_ip = $_ENV['REMOTE_ADDR'];
+		} else {
+			$client_ip = 'unknown';
+		}
+	}
+	return $client_ip;
+}
